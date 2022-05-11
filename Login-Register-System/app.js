e45fc13df536a3a1bc10d7a8b6af03e9da5c38f3
michaelgeorgisva <michael.georgisva@student.president.ac.id>
2022-05-11 16:27:08 +0700

    minor update

--- Login-Register-System/app.js
+++ Login-Register-System/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const createError = require('http-errors');
+const cors = require('cors');
 const routes = require('./routes/routes');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
