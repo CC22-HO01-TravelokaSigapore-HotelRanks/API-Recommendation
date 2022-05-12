d312d8e4e9c92f0dcb00a7b18fce0cebf89ebc5c
michaelgeorgisva <michael.georgisva@student.president.ac.id>
2022-05-12 23:01:49 +0700

    minor update

--- Login-Register-System/db/dbConnection.js
+++ Login-Register-System/db/dbConnection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   dialect: dbConfig.dialect,
   pool: dbConfig.pool,
   dialectOptions: {
-    socketPath: dbConfig.SQSLINSTANCE,
+    socketPath: dbConfig.SQLINSTANCE,
   },
 });
 
