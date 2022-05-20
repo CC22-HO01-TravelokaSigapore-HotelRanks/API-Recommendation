5700dc70b52879b0838f64c86d3d65ffab499669
Alvin-Buana <alvinbuanaa@gmail.com>
2022-05-20 13:30:31 +0700

    seperate search endpoint

--- API-Hotel/routes/hotel.js
+++ API-Hotel/routes/hotel.js
@@ -118,57 +118,57 @@ router.get('/list',async (req, res) => {
     }
 }
 );
-router.get('/search',async (req, res) => {
-    let pages = 10
-    let offset = 0
-    if (req.query.page){
-        pages = parseInt(req.query.page)
-    }
-    if (req.query.offset){
-        offset = parseInt(req.query.offset)
-    }
-    try{
-        const info = req.query.keyword;
-        if (info){
-            const Hotel = await hotel.findAll({
-                limit: pages,
-                offset: offset,
-                where: {
-                    [Op.or]:[
-                        {
-                        name: {[Op.like]: `%${info}%`}
-                    },{   
-                         neighborhood: {[Op.like]: `%${info}%`}
-                    },{
-                        type_nearby_destination: {[Op.like]: `%${info}%`},
-                    }
-                    ]
-                }
-            });
-            if (Hotel.length > 0) {
-                return res.status(200).json({
-                    message: 'Success',
-                    data: Hotel
-                });
-            } else {
-                return res.status(200).json({
-                    message: 'No hotel found'
-                });
-            }
-        }
-        else{
-            return res.status(200).json({
-                message: 'Please enter a hotel'
-            });
-        }
+// router.get('/search',async (req, res) => {
+//     let pages = 10
+//     let offset = 0
+//     if (req.query.page){
+//         pages = parseInt(req.query.page)
+//     }
+//     if (req.query.offset){
+//         offset = parseInt(req.query.offset)
+//     }
+//     try{
+//         const info = req.query.keyword;
+//         if (info){
+//             const Hotel = await hotel.findAll({
+//                 limit: pages,
+//                 offset: offset,
+//                 where: {
+//                     [Op.or]:[
+//                         {
+//                         name: {[Op.like]: `%${info}%`}
+//                     },{   
+//                          neighborhood: {[Op.like]: `%${info}%`}
+//                     },{
+//                         type_nearby_destination: {[Op.like]: `%${info}%`},
+//                     }
+//                     ]
+//                 }
+//             });
+//             if (Hotel.length > 0) {
+//                 return res.status(200).json({
+//                     message: 'Success',
+//                     data: Hotel
+//                 });
+//             } else {
+//                 return res.status(200).json({
+//                     message: 'No hotel found'
+//                 });
+//             }
+//         }
+//         else{
+//             return res.status(200).json({
+//                 message: 'Please enter a hotel'
+//             });
+//         }
         
-    }catch(err){
-        return res.status(404).json({
-            message: err
-        });
-    }
-}
-);
+//     }catch(err){
+//         return res.status(404).json({
+//             message: err
+//         });
+//     }
+// }
+// );
 router.get('/:rating',async (req, res) => {
     try{
         const Hotel = await hotel.findAll({
