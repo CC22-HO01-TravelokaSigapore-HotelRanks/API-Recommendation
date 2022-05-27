b4d8add258e97b48cca0687019e57f45bda324a3
michaelgeorgisva <michael.georgisva@student.president.ac.id>
2022-05-27 13:02:44 +0700

    directory changes on 'Review-Endpoint'

--- Review-Endpoint/app.js
+++ Review-Endpoint/app.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const db = require("./app/models");
+const db = require("./models/index");
 
 const corsOptions = {
   origin: "*"
 };
 app.use(cors(corsOptions));
-// parse requests of content-type - application/json
+
 app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-// simple route
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Review-Endpoint application." });
 });
-require("./app/routes/routes")(app);
+
+require("./routes/routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
